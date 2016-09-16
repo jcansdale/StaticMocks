0f51d23d68f5a5128a296dc3df560b92c5687af5
Jamie Cansdale <jcansdale@gmail.com>
2016-09-16 22:47:32 +0100

    Added support for fast Func delegates when using NSubstitute.

--- src/StaticMocks/NSubstituteSupport.cs
+++ src/StaticMocks/NSubstituteSupport.cs
@@ -4,10 +4,32 @@
     using System;
     using System.Collections.Generic;
     using System.Linq.Expressions;
+    using System.Reflection;
     using ExpressionUtilities = StaticMock.ExpressionUtilities;
 
     public static class NSubstituteSupport
     {
+        public static R For<R>(this StaticMock staticMock, Expression<Func<R>> target)
+        {
+            var methodCallExpression = (MethodCallExpression)target.Body;
+            var method = methodCallExpression.Method;
+
+            var mockDelegate = staticMock.GetMockDelegate(method);
+            if (!mockDelegate.IsMocked)
+            {
+                mockDelegate.Delegate = createDelegateFor(mockDelegate.DelegateType);
+            }
+
+            var argList = new List<object>();
+            foreach (var arg in methodCallExpression.Arguments)
+            {
+                var value = ExpressionUtilities.GetValue(arg);
+                argList.Add(value);
+            }
+
+            return (R)mockDelegate.Delegate.DynamicInvoke(argList.ToArray());
+        }
+
         public static object For(this StaticMock staticMock, Expression<Action> target)
         {
             var methodCallExpression = (MethodCallExpression)target.Body;
@@ -27,12 +49,81 @@
             }
 
             mockDelegate.Delegate.DynamicInvoke(argList.ToArray());
-            return null;
+            var action = mockDelegate.Delegate.Target;
+            action.ClearReceivedCalls();
+            return action;
         }
 
         static Delegate createDelegateFor(Type delegateType)
         {
+            var dele = quickCreateDelegateFor(delegateType);
+            if(dele != null)
+            {
+                return dele;
+            }
+
             return (Delegate)Substitute.For(new Type[] { delegateType }, new object[0]);
         }
+
+        // TODO: Add support for all Actions and Funcs.
+        static Delegate quickCreateDelegateFor(Type delegateType)
+        {
+            Type type;
+            switch (delegateType.Name)
+            {
+                case "Action":
+                    type = typeof(IAction);
+                    break;
+                case "Action`1":
+                    type = typeof(IAction<>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`1":
+                    type = typeof(IFunc<>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`2":
+                    type = typeof(IFunc<,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`3":
+                    type = typeof(IFunc<,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`4":
+                    type = typeof(IFunc<,,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`5":
+                    type = typeof(IFunc<,,,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`6":
+                    type = typeof(IFunc<,,,,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`7":
+                    type = typeof(IFunc<,,,,,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`8":
+                    type = typeof(IFunc<,,,,,,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                case "Func`9":
+                    type = typeof(IFunc<,,,,,,,,>).MakeGenericType(delegateType.GenericTypeArguments);
+                    break;
+                default:
+                    throw new Exception("Unsupported delegate type: " + delegateType.Name);
+                    //return null;
+            }
+
+            var target = Substitute.For(new Type[] { type }, new object[0]);
+            var invokeMethod = type.GetTypeInfo().GetMethod("Invoke");
+            return invokeMethod.CreateDelegate(delegateType, target);
+        }
+
+        public interface IAction { void Invoke(); }
+        public interface IAction<T> { void Invoke(T t); }
+        public interface IFunc<R> { R Invoke(); }
+        public interface IFunc<T, R> { R Invoke(T t); }
+        public interface IFunc<T1, T2, R> { R Invoke(T1 t1, T2 t2); }
+        public interface IFunc<T1, T2, T3, R> { R Invoke(T1 t1, T2 t2, T3 t3); }
+        public interface IFunc<T1, T2, T3, T4, R> { R Invoke(T1 t1, T2 t2, T3 t3, T4 t4); }
+        public interface IFunc<T1, T2, T3, T4, T5, R> { R Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5); }
+        public interface IFunc<T1, T2, T3, T4, T5, T6, R> { R Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6); }
+        public interface IFunc<T1, T2, T3, T4, T5, T6, T7, R> { R Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7); }
+        public interface IFunc<T1, T2, T3, T4, T5, T6, T7, T8, R> { R Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T7 t8); }
     }
 }
---
+++ test/StaticMocks.Tests/NSubstituteSupportTests.cs
@@ -0,0 +1,225 @@
+﻿namespace StaticMocks.Tests.StaticMocks.Tests
+{
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+
+    public class NSubstituteSupportTests
+    {
+        StaticMock staticMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            staticMock = new StaticMock(GetType());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            staticMock.Dispose();
+        }
+
+        [Test]
+        public void For_Action0()
+        {
+            var action = staticMock.For(() => Tests.StaticClass.ActionMethod());
+
+            StaticClass.ActionMethod0();
+
+            action.Received(1);
+            staticMock.For(() => Tests.StaticClass.ActionMethod());
+        }
+
+        [Test]
+        public void For_Action1()
+        {
+            var action = staticMock.For(() => Tests.StaticClass.ActionMethod(Arg.Any<int>()));
+
+            StaticClass.ActionMethod1(1);
+
+            action.Received(1);
+            staticMock.For(() => Tests.StaticClass.ActionMethod(1));
+        }
+
+        [Test]
+        public void For_Func1_ExpectReturns()
+        {
+            var expectText = "__ExpectText1__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod()).Returns(expectText);
+
+            var text = StaticClass.FuncMethod0();
+            StaticClass.FuncMethod0();
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func2_ExpectReturns()
+        {
+            var expectText = "__ExpectText2__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod1(1);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func3_ExpectReturns()
+        {
+            var expectText = "__ExpectText3__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod2(1, 2);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func4_ExpectReturns()
+        {
+            var expectText = "__ExpectText4__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2, 3)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod3(1, 2, 3);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func5_ExpectReturns()
+        {
+            var expectText = "__ExpectText5__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2, 3, 4)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod4(1, 2, 3, 4);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func6_ExpectReturns()
+        {
+            var expectText = "__ExpectText6__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2, 3, 4, 5)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod5(1, 2, 3, 4, 5);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func7_ExpectReturns()
+        {
+            var expectText = "__ExpectText7__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2, 3, 4, 5, 6)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod6(1, 2, 3, 4, 5, 6);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func8_ExpectReturns()
+        {
+            var expectText = "__ExpectText8__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2, 3, 4, 5, 6, 7)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod7(1, 2, 3, 4, 5, 6, 7);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void For_Func9_ExpectReturns()
+        {
+            var expectText = "__ExpectText9__";
+            staticMock.For(() => Tests.StaticClass.FuncMethod(1, 2, 3, 4, 5, 6, 7, 8)).Returns(expectText);
+
+            var text = StaticClass.FuncMethod8(1, 2, 3, 4, 5, 6, 7, 8);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void ReadAllText_MockReturns_ExpectText()
+        {
+            var path = "__PATH__";
+            var expectText = "__EXPECT_TEXT__";
+            staticMock.For(() => System.IO.File.ReadAllText(path)).Returns(expectText);
+
+            var text = File.ReadAllText(path);
+
+            Assert.That(text, Is.EqualTo(expectText));
+        }
+
+        [Test]
+        public void Setup_Twice_ExpectBoth()
+        {
+            var path1 = "__PATH1__";
+            var path2 = "__PATH2__";
+            var expect1 = "__EXPECT1__";
+            var expect2 = "__EXPECT2__";
+
+            staticMock.For(() => System.IO.File.ReadAllText(path1)).Returns(expect1);
+            staticMock.For(() => System.IO.File.ReadAllText(path2)).Returns(expect2);
+
+            var text1 = File.ReadAllText(path1);
+            var text2 = File.ReadAllText(path2);
+            Assert.That(text1, Is.EqualTo(expect1));
+            Assert.That(text2, Is.EqualTo(expect2));
+        }
+
+        [Test]
+        public void Setup_Multi_ExpectBoth()
+        {
+            var path1 = "__PATH1__";
+            var expect1 = "__EXPECT1__";
+            var expect2 = "__EXPECT2__";
+
+            staticMock.For(() => System.IO.File.ReadAllText(path1)).Returns(expect1, expect2);
+
+            var text1 = File.ReadAllText(path1);
+            var text2 = File.ReadAllText(path1);
+            Assert.That(text1, Is.EqualTo(expect1));
+            Assert.That(text2, Is.EqualTo(expect2));
+        }
+
+        partial class StaticClass
+        {
+            internal static Action ActionMethod0 = () => Tests.StaticClass.ActionMethod();
+            internal static Action<int> ActionMethod1 = (t1) => Tests.StaticClass.ActionMethod(t1);
+
+            internal static Func<string> FuncMethod0 = () => Tests.StaticClass.FuncMethod();
+            internal static Func<int, string> FuncMethod1 = (t1) => Tests.StaticClass.FuncMethod(t1);
+            internal static Func<int, int, string> FuncMethod2 = (t1, t2) => Tests.StaticClass.FuncMethod(t1, t2);
+            internal static Func<int, int, int, string> FuncMethod3 = (t1, t2, t3) => Tests.StaticClass.FuncMethod(t1, t2, t3);
+            internal static Func<int, int, int, int, string> FuncMethod4 = (t1, t2, t3, t4) => Tests.StaticClass.FuncMethod(t1, t2, t3, t4);
+            internal static Func<int, int, int, int, int, string> FuncMethod5 = (t1, t2, t3, t4, t5) => Tests.StaticClass.FuncMethod(t1, t2, t3, t4, t5);
+            internal static Func<int, int, int, int, int, int, string> FuncMethod6 = (t1, t2, t3, t4, t5, t6) => Tests.StaticClass.FuncMethod(t1, t2, t3, t4, t5, t6);
+            internal static Func<int, int, int, int, int, int, int, string> FuncMethod7 = (t1, t2, t3, t4, t5, t6, t7) => Tests.StaticClass.FuncMethod(t1, t2, t3, t4, t5, t6, t7);
+            internal static Func<int, int, int, int, int, int, int, int, string> FuncMethod8 = (t1, t2, t3, t4, t5, t6, t7, t8) => Tests.StaticClass.FuncMethod(t1, t2, t3, t4, t5, t6, t7, t8);
+        }
+
+        partial class File
+        {
+            internal static Func<string, string> ReadAllText = (string path) => System.IO.File.ReadAllText(path);
+        }
+    }
+
+    static class StaticClass
+    {
+        internal static void ActionMethod() { }
+        internal static void ActionMethod(int t) { }
+        internal static string FuncMethod() => "FuncMethod";
+        internal static string FuncMethod(int t1) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2, int t3) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2, int t3, int t4) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2, int t3, int t4, int t5) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2, int t3, int t4, int t5, int t6) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2, int t3, int t4, int t5, int t6, int t7) => "FuncMethod";
+        internal static string FuncMethod(int t1, int t2, int t3, int t4, int t5, int t6, int t7, int t8) => "FuncMethod";
+    }
+}
