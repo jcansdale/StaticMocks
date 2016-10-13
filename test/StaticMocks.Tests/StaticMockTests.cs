50b8f68960ce5848ee465f1d42025127fd4933ca
Jamie Cansdale <jcansdale@gmail.com>
2016-10-13 23:48:29 +0100

    Check no exception is thrown if `StaticMock.Dispose` is called more than once.

--- test/StaticMocks.Tests/StaticMockTests.cs
+++ test/StaticMocks.Tests/StaticMockTests.cs
@@ -45,6 +45,15 @@
                 Assert.That(mockDelegate.IsMocked, Is.True);
             }
         }
+
+        [Test]
+        public void Dispose_CalledMoreThanOnce_DoesNotThrow()
+        {
+            using (var staticMock = new StaticMock(typeof(TargetNamespace.TargetClass)))
+            {
+                Assert.DoesNotThrow(() => staticMock.Dispose());
+            }
+        }
     }
 
     namespace StaticNamespace
